549416058794eb7ea377f46ff2c13fca62bbfd6e
JaneLMF <long.meifeng@pivosgroup.com.cn>
2017-10-18 20:37:51 +0800

    + add easypermissions

--- app/src/main/java/com/txt/conference/activity/BaseActivity.kt
+++ app/src/main/java/com/txt/conference/activity/BaseActivity.kt
@@ -1,9 +1,23 @@
 package com.txt.conference.activity
 
 import android.app.Activity
+import pub.devrel.easypermissions.EasyPermissions
 
 /**
  * Created by jane on 2017/10/9.
  */
-open class BaseActivity : Activity() {
+open class BaseActivity : Activity(), EasyPermissions.PermissionCallbacks {
+
+    override fun onPermissionsDenied(requestCode: Int, perms: MutableList<String>?) {
+
+    }
+
+    override fun onPermissionsGranted(requestCode: Int, perms: MutableList<String>?) {
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this)
+    }
+
 }
